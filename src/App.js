4e9ad8fb8b348e179aa882ed2a3b5196b4e95456
Loi Van Tran <datloiboi@gmail.com>
2021-03-17 13:51:37 +0700

    WIP wk7

--- src/App.js
+++ src/App.js
@@ -10,29 +10,36 @@ function App() {
   const dispatch = useDispatch();
   const countState = useSelector((state) => state.count);
   const authState = useSelector((state) => state.auth);
-  console.log({ authState });
 
   return (
     <div className="App">
       <Navbar />
       <Container fluid>
         <Row>
-          <Col className="bg-success d-flex justify-content-center p-3">
-            <input
-              className="mr-3"
-              onChange={(e) =>
-                dispatch({ type: "CHANGE_COLOR", payload: e.target.value })
-              }
-            ></input>
-            <button
-              className="mr-3"
-              onClick={() => dispatch({ type: "INCREMENT" })}
-            >
-              Increment
-            </button>
-            <button onClick={() => dispatch({ type: "DECREMENT" })}>
-              Decrement
-            </button>
+          <Col className="bg-success d-flex flex-column align-items-center justify-content-center p-3">
+            <div>
+              <h1>{countState.count}</h1>
+            </div>
+            <div className="d-flex align-items-center justify-content-center">
+              <input
+                className="mr-3"
+                onChange={(e) =>
+                  dispatch({ type: "CHANGE_COLOR", payload: e.target.value })
+                }
+              ></input>
+              <button
+                className="m-1 btn btn-secondary btn-block"
+                onClick={() => dispatch({ type: "INCREMENT" })}
+              >
+                Increment
+              </button>
+              <button
+                className="m-1 btn btn-secondary btn-block"
+                onClick={() => dispatch({ type: "DECREMENT" })}
+              >
+                Decrement
+              </button>
+            </div>
           </Col>
         </Row>
         <Row>
@@ -48,25 +55,28 @@ function App() {
               return (
                 <div
                   key={idx}
-                  className="d-flex justify-content-around rounded my-1 p-4"
+                  className="d-flex justify-content-around rounded my-1 p-4 border"
                   style={{ backgroundColor: boxColor }}
                 >
-                  <div className="mb-3">
-                    <h1>Box {idx}</h1>
+                  <div className="w-100">
+                    <h1 className="font-weight-bold">Box {idx}</h1>
                     {email}
                   </div>
-                  <input
-                    onChange={(e) =>
-                      dispatch({
-                        type: "CHANGE_SPECIFIC_BOX",
-                        payload: {
-                          index: idx,
-                          color: e.target.value,
-                          email: authState.email,
-                        },
-                      })
-                    }
-                  ></input>
+                  <div className="w-100 d-flex align-items-center">
+                    <input
+                      className="form-control"
+                      onChange={(e) =>
+                        dispatch({
+                          type: "CHANGE_SPECIFIC_BOX",
+                          payload: {
+                            index: idx,
+                            color: e.target.value,
+                            email: authState.email,
+                          },
+                        })
+                      }
+                    ></input>
+                  </div>
                 </div>
               );
             })}
