eb0ce58a168d8428e96a846ee5b8d8b49d7f712f
Loi Van Tran <datloiboi@gmail.com>
2021-03-17 12:30:09 +0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import { Container, Row, Col } from "react-bootstrap";
+
+import { useSelector, useDispatch } from "react-redux";
+
+import "./App.css";
 
 function App() {
+  const dispatch = useDispatch();
+  const state = useSelector((state) => state);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App p-3">
+      <Container fluid>
+        <Row>
+          <Col className="bg-success d-flex justify-content-center p-3">
+            <input
+              className="mr-3"
+              onChange={(e) =>
+                dispatch({ type: "CHANGE_COLOR", payload: e.target.value })
+              }
+            ></input>
+            <button
+              className="mr-3"
+              onClick={() => dispatch({ type: "INCREMENT" })}
+            >
+              Increment
+            </button>
+            <button onClick={() => dispatch({ type: "DECREMENT" })}>
+              Decrement
+            </button>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            {Array.from(Array(state.count)).map((c, idx) => {
+              const boxColor = state.indexColors[idx] || state.defaultColor;
+              return (
+                <div
+                  key={idx}
+                  className="d-flex justify-content-around rounded my-1 p-4"
+                  style={{ backgroundColor: boxColor }}
+                >
+                  Box {idx}
+                  <input
+                    onChange={(e) =>
+                      dispatch({
+                        type: "CHANGE_SPECIFIC_BOX",
+                        payload: { color: e.target.value, index: idx },
+                      })
+                    }
+                  ></input>
+                </div>
+              );
+            })}
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,57 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import reportWebVitals from "./reportWebVitals";
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const initialState = {
+  count: 10,
+  indexColors: [null],
+  defaultColor: "rgb(215, 71, 66)",
+};
+
+export default function countReducer(state = initialState, action) {
+  switch (action.type) {
+    case 'CHANGE_COLOR':
+      return {
+        ...state,
+        defaultColor: action.payload
+      }
+    case "CHANGE_SPECIFIC_BOX":
+      const indexColors = state.indexColors
+      indexColors[action.payload.index] = action.payload.color
+      return {
+        ...state,
+        indexColors: indexColors
+      }
+    case "INCREMENT":
+      return {
+        ...state,
+        count: state.count + 1,
+      };
+    case "DECREMENT":
+      return {
+        ...state,
+        count: state.count - 1
+      };
+    default:
+      return state;
+  }
+}
+
+const store = createStore(countReducer);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
